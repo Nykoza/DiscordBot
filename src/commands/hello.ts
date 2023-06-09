7392c22fd72a0af91e74541ed4f27a6ce8877e4c
Nykoza <nicolas.goepp@epitech.eu>
2023-06-10 01:36:51 +0200

    Options choices for slashCommand

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -4,7 +4,18 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 export const hello: Command = {
   data: new SlashCommandBuilder()
     .setName("hello")
-    .setDescription("Hello world description"),
+    .setDescription("Hello world description")
+    .addStringOption((option) => {
+      return option
+        .setName("choix")
+        .setDescription("Papier/Caillou/Ciseaux")
+        .setRequired(true)
+        .setChoices(
+          { name: "Papier", value: "paper" },
+          { name: "Caillou", value: "rock" },
+          { name: "Ciseau", value: "scissors" }
+        );
+    }),
   run: async (interaction) => {
     await interaction.reply({ content: "Hello world", ephemeral: true });
   },
